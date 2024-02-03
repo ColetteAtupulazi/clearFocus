e75192ebe2e68a221df90983473ca46b5a0c4c62
Colette <colette.atupulazi@my.utsa.edu>
2024-02-03 10:37:42 -0600

    starter code

---
+++ app.js
@@ -0,0 +1,23 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() === '') {
+        alert('Please enter a task.');
+        return;
+    }
+
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = `
+        <span>${taskInput.value}</span>
+        <button onclick="removeTask(this)">Remove</button>
+    `;
+
+    taskList.appendChild(taskItem);
+    taskInput.value = '';
+}
+
+function removeTask(button) {
+    const taskItem = button.parentElement;
+    taskItem.remove();
+}
